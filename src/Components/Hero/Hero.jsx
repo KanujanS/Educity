6fec96258573bd80dafbea58bde52fafe2924c7a
KanujanS <kanujansk@gmail.com>
2025-02-17 21:43:30 +0530

    Code initialization

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='Hero Container'>
+        <div className='hero-text'>
+        <h1>We ensure better education for a better world</h1>
+        <p>Our cutting edge curriculam is designed to empower students with the konwledge, skills, and experiences needed to excel in the dynamic field of education</p>
+        <button className='btn'>Explore more</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
