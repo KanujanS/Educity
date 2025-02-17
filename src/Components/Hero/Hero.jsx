4ae7426ff563848ccef1b43151840f2a54cd91a9
KanujanS <kanujansk@gmail.com>
2025-02-17 22:17:11 +0530

    code update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='Hero Container'>
         <div className='hero-text'>
-        <h1>We ensure better education for a better world it is Kanujan</h1>
+        <h1>We ensure better education for a better world</h1>
         <p>Our cutting edge curriculam is designed to empower students with the konwledge, skills, and experiences needed to excel in the dynamic field of education</p>
         <button className='btn'>Explore more</button>
         </div>
