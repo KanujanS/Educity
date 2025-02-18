a47f23a95054903c4814eb8107033d5f91ef1e91
KanujanS <kanujansk@gmail.com>
2025-02-18 11:01:39 +0530

    code updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className='Container'>
+        <Title subtitle="What we offer" title="Our programs"/>
+        <Programs/>
+        <About/>
+        <Title subtitle="Campus Photos" title="Gallery"/>
+        <Campus/>
+        <Title subtitle="What students says" title="Testimonials"/>
+      </div>
     </div>
   )
 }
---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Programs.css'
+import Program_1 from '../../assets/Program-1.png'
+import Program_2 from '../../assets/Program-2.png'
+import Program_3 from '../../assets/Program-3.png'
+import Program_icon_1 from '../../assets/Program-icon-1.png'
+import Program_icon_2 from '../../assets/Program-icon-2.png'
+import Program_icon_3 from '../../assets/Program-icon-3.png'
+
+const Programs = () => {
+  return (
+    <div className='Programs'>
+        <div className='Program'>
+            <img src={Program_1} alt="" />
+            <div className="caption">
+            <img src={Program_icon_1} alt="" />
+            <p>Graduation degree</p>
+            </div>
+        </div>
+        <div className='Program'>
+            <img src={Program_2} alt="" />
+            <div className="caption">
+            <img src={Program_icon_2} alt="" />
+            <p>Masters degree</p>
+            </div>
+        </div>
+        <div className='Program'>
+            <img src={Program_3} alt="" />
+            <div className="caption">
+            <img src={Program_icon_3} alt="" />
+            <p>Postgraduation Graduation</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Programs
