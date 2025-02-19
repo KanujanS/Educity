3c0dd4076870f1476386022e963a4566f2af3334
KanujanS <kanujansk@gmail.com>
2025-02-19 22:08:31 +0530

    code updates

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,38 +1,40 @@
-import React from 'react'
-import './Programs.css'
-import Program_1 from '../../assets/Program-1.png'
-import Program_2 from '../../assets/Program-2.png'
-import Program_3 from '../../assets/Program-3.png'
-import Program_icon_1 from '../../assets/Program-icon-1.png'
-import Program_icon_2 from '../../assets/Program-icon-2.png'
-import Program_icon_3 from '../../assets/Program-icon-3.png'
+import React from 'react';
+import './Programs.css';
+import Program1 from '../../assets/Program-1.png';
+import Program2 from '../../assets/Program-2.png';
+import Program3 from '../../assets/Program-3.png';
+import ProgramIcon1 from '../../assets/Program-icon-1.png';
+import ProgramIcon2 from '../../assets/Program-icon-2.png';
+import ProgramIcon3 from '../../assets/Program-icon-3.png';
 
 const Programs = () => {
   return (
     <div className='Programs'>
-        <div className='Program'>
-            <img src={Program_1} alt="" />
-            <div className="caption">
-            <img src={Program_icon_1} alt="" />
-            <p>Graduation degree</p>
-            </div>
+      <div className='Program'>
+        <img src={Program1} alt="Graduation Degree Program" />
+        <div className="caption">
+          <img src={ProgramIcon1} alt="Graduation Icon" />
+          <p>Graduation Degree</p>
         </div>
-        <div className='Program'>
-            <img src={Program_2} alt="" />
-            <div className="caption">
-            <img src={Program_icon_2} alt="" />
-            <p>Masters degree</p>
-            </div>
+      </div>
+
+      <div className='Program'>
+        <img src={Program2} alt="Master's Degree Program" />
+        <div className="caption">
+          <img src={ProgramIcon2} alt="Master's Icon" />
+          <p>Master's Degree</p>
         </div>
-        <div className='Program'>
-            <img src={Program_3} alt="" />
-            <div className="caption">
-            <img src={Program_icon_3} alt="" />
-            <p>Postgraduation Graduation</p>
-            </div>
+      </div>
+
+      <div className='Program'>
+        <img src={Program3} alt="Postgraduate Degree Program" />
+        <div className="caption">
+          <img src={ProgramIcon3} alt="Postgraduate Icon" />
+          <p>Postgraduate Degree</p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Programs
+export default Programs;
